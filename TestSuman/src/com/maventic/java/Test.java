1c101bbc8eabbe1a01999c34b050364f76ec0729
mm0415 <mm0415@MMDT0181.maven.local>
2018-06-22 12:52:58 +0530

    tested

--- TestSuman/src/com/maventic/java/Test.java
+++ TestSuman/src/com/maventic/java/Test.java
@@ -3,6 +3,6 @@ package com.maventic.java;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		System.out.println("Hellhubjnkjgo World !");
 	}
 }
